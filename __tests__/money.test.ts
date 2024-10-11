aa326e324d0a4fac04eb5c99ec3243225fa71f38
aki-kun-desu <aki.kun.desu.123@gmail.com>
2024-10-11 22:57:18 +0900

    chapter.8-3

--- __tests__/money.test.ts
+++ __tests__/money.test.ts
@@ -4,20 +4,20 @@ import { Money } from "../src/money";
 
 test("$5 * 2 = $10", () => {
   const five = Money.dollar(5);
-  expect(five.times(2).getAmount()).toEqual(Dollar(10).getAmount());
-  expect(five.times(3).getAmount()).toEqual(Dollar(15).getAmount());
+  expect(five.times(2).getAmount()).toEqual(Money.dollar(10).getAmount());
+  expect(five.times(3).getAmount()).toEqual(Money.dollar(15).getAmount());
 });
 
 test("equals()", () => {
-  expect(Dollar(5).equals(Dollar(5))).toBe(true);
-  expect(Dollar(5).equals(Dollar(6))).toBe(false);
-  expect(Franc(5).equals(Franc(5))).toBe(true);
-  expect(Franc(5).equals(Franc(6))).toBe(false);
-  expect(Dollar(5).equals(Franc(5))).toBe(false);
+  expect(Money.dollar(5).equals(Money.dollar(5))).toBe(true);
+  expect(Money.dollar(5).equals(Money.dollar(6))).toBe(false);
+  expect(Money.franc(5).equals(Money.franc(5))).toBe(true);
+  expect(Money.franc(5).equals(Money.franc(6))).toBe(false);
+  expect(Money.dollar(5).equals(Money.franc(5))).toBe(false);
 });
 
 test("5CHF * 2 = 10CHF", () => {
   const five = Money.franc(5);
-  expect(five.times(2).getAmount()).toEqual(Franc(10).getAmount());
-  expect(five.times(3).getAmount()).toEqual(Franc(15).getAmount());
+  expect(five.times(2).getAmount()).toEqual(Money.franc(10).getAmount());
+  expect(five.times(3).getAmount()).toEqual(Money.franc(15).getAmount());
 });
